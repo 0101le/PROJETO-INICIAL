cd7f158da96073f1513750666c7e23d09eebbea3
0101le <leticiamakepop@gmail.com>
2024-03-15 08:24:09 -0300

    Variavel nome

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar (event){
-  alert("você clicou no botão!!" + event)
+  alert("você clicou no botão!!" + " " + nome.value);
 
   var numero = 6;
   var resultado = numeroo / 2;
