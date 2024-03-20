13e4335b3f5b67cc9b06690bee71bfca008dfe6f
0101le <leticiamakepop@gmail.com>
2024-03-20 09:58:12 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -2,12 +2,34 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
 
-function alertar (event){
-  alert("você clicou no botão!!" + " " + nome.value);
+var email = document.getElementById("email");
 
-  var numero = 6;
-  var resultado = numeroo / 2;
-  if(resultado == 0){
-    alert("este numero é par!");
-  }
+var Endereço= document.getElementById("Endereço");
+
+var Telefone = document.getElementById("Telefone");
+
+var Cep= document.getElementById("Cep");
+
+var Complemento = document.getElementById("complemento");
+
+var Bairro = document.getElementById("Bairro");
+
+var Estado = document.getElementById("Estado");
+
+var saída = document.getElementById("saída de dados");
+
+
+
+
+function alertar (event){
+  //alert("você clicou no botão!!" + " " + nome.value);
+
+  //var numero = 6;
+ // var resultado = numero % / 2;
+  //if(resultado == 0){
+   // alert("este numero é par!");
+  
+  Saida.innerText = "nome: " + nome.value +
+  "\n email: " + email.value + "\nEndereço: " + Telefone.value
+
 }
